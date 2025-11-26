ab47d46d2b0818ec63b2e51d22bc23357ef0f26b
Heo Ik <heoi@ais-info.co.jp>
2025-11-26 22:22:47 +0900

    init

---
+++ src/main/java/com/project/base/BaseTest.java
@@ -0,0 +1,30 @@
+package com.project.base;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        // 1) 브라우저 선택 (나중에는 config 파일에서 읽도록 확장 가능)
+        String browser = "chrome";
+
+        // 2) DriverFactory 호출
+        driver = DriverFactory.createDriver(browser);
+
+        // 3) 공통 설정
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
---
+++ src/main/java/com/project/base/DriverFactory.java
@@ -0,0 +1,34 @@
+package com.project.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class DriverFactory {
+
+    public static WebDriver createDriver(String browser) {
+
+        switch (browser.toLowerCase()) {
+
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--start-maximized");
+                return new ChromeDriver(options);
+
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return new FirefoxDriver();
+
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                return new EdgeDriver();
+
+            default:
+                throw new IllegalArgumentException("지원하지 않는 브라우저입니다: " + browser);
+        }
+    }
+}
